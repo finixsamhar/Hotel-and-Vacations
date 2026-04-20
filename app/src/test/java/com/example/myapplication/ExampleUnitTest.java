67390ed5c1fb1ad3f078cb5793ea34aaa58e5bb9
aissam <aissambouguerfa@gmail.com>
2026-04-19 21:32:13 -0400

    Task D: JUnit testing for date

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void vacationDateIsCorrect() {
+// Mock data
+        long start = 1714521600000L; // May 1, 2024
+        long end = 1714435200000L; // April 30, 2024 (Invalid because it's before start)
+
+// The test condition: end should NOT be before start
+        assertTrue("End date should be after start date", end > start);
     }
 }
